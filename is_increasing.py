4bf210f34e7885230571fab470ac5400b8f9c0d5
Nikolai Petkov <binana@abv.bg>
2015-03-18 14:02:02 +0200

    Done increasing sequence

---
+++ is_increasing.py
@@ -0,0 +1,14 @@
+def is_increasing(seq):
+    if (len(seq) < 2):
+        return True
+    for i in range(len(seq) - 1):
+        if (seq[i] >= seq[i + 1]):
+            return False
+    return True
+
+
+if __name__ == '__main__':
+    print(is_increasing([1, 2, 3, 4, 5]))
+    print(is_increasing([1]))
+    print(is_increasing([5, 6, -10]))
+    print(is_increasing([1, 1, 1, 1]))
