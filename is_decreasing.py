69fc0610209ea4d2c16b4b93106d90f45e0d5b8d
Nikolai Petkov <binana@abv.bg>
2015-03-18 14:05:21 +0200

    Done decreasing sequence

---
+++ is_decreasing.py
@@ -0,0 +1,14 @@
+def is_decreasing(seq):
+    if (len(seq) < 2):
+        return True
+    for i in range(len(seq) - 1):
+        if (seq[i] <= seq[i + 1]):
+            return False
+    return True
+
+
+if __name__ == '__main__':
+    print(is_decreasing([5, 4, 3, 2, 1]))
+    print(is_decreasing([1, 2, 3]))
+    print(is_decreasing([100, 50, 20]))
+    print(is_decreasing([1, 1, 1, 1]))
