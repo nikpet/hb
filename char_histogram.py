c82b7265ad3705484debf83861586aef57eea49f
Nikolai Petkov <binana@abv.bg>
2015-03-18 13:08:42 +0200

    Done char histogram

---
+++ char_histogram.py
@@ -0,0 +1,12 @@
+def char_histogram(string):
+    histogram = {}
+    for letter in string:
+        if letter in histogram:
+            histogram[letter] = histogram[letter] + 1
+        else:
+            histogram[letter] = 1
+    return histogram
+
+if __name__ == '__main__':
+    print(char_histogram("Python!"))
+    print(char_histogram("AAAAaaa!!!"))
