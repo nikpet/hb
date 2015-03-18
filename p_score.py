5a6064cad3e813261fba298b4e08c82891c3ee4a
Nikolai Petkov <binana@abv.bg>
2015-03-18 13:48:58 +0200

    Done palindrome score

---
+++ p_score.py
@@ -0,0 +1,22 @@
+from palindrom import palindrome
+
+
+def p_score(n):
+    calculate(n, 0)
+
+
+def calculate(n, score=0):
+    if palindrome(n):
+        score = score + 1
+        print(score)
+    else:
+        score = score + 1
+        reverse_number = str(n)[::-1]
+        number = n + int(reverse_number)
+        calculate(number, score)
+
+
+if __name__ == '__main__':
+    p_score(121)
+    p_score(48)
+    p_score(198)
