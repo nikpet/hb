c27cd0d564912b0fab47fc6e80d5d962280d94bb
Nikolai Petkov <binana@abv.bg>
2015-04-02 08:22:20 +0300

    commit with last changes

--- week3/2/panda_social_network.py
+++ week3/2/panda_social_network.py
@@ -33,3 +33,30 @@ class PandaSocialNetwork:
             return False
         return self.network[panda]
 
+    def _find_shortest_path(self, panda1, panda2, path=[]):
+        path = path + [panda1]
+        if panda1 == panda2:
+            return path
+        if panda1 not in self.network:
+            return None
+        shortest = None
+        for node in self.network[panda1]:
+            if node not in path:
+                newpath = self._find_shortest_path(node, panda2, path)
+                if newpath:
+                    if not shortest or len(newpath) < len(shortest):
+                        shortest = newpath
+        return shortest
+
+    def connection_level(self, panda1, panda2):
+        path = self._find_shortest_path(panda1, panda2)
+        if path:
+            return len(path) - 1
+        else:
+            return -1
+
+    def are_connected(self, panda1, panda2):
+        if self.connection_level(panda1, panda2) > 0:
+            return True
+        else:
+            return False
--- week3/2/panda_social_network_test.py
+++ week3/2/panda_social_network_test.py
@@ -1,5 +1,5 @@
 import unittest
-from panda_social_netword import PandaSocialNetwork
+from panda_social_network import PandaSocialNetwork
 from panda import Panda, PandaAlreadyThere
 
 
@@ -8,8 +8,9 @@ class PandaSocialNetworkTest(unittest.TestCase):
         self.network = PandaSocialNetwork()
         self.panda = Panda('name', 'e@mail.cc', 'male')
 
-    def _female_panda(self):
-        return Panda('name', 'm@ail.cc', 'female')
+    def _new_panda(self, index):
+        return Panda('name' + str(index), 'm@ail.cc', 'female')
+
 
     def test_init(self):
         self.assertEqual(self.network.network, {})
@@ -21,34 +22,55 @@ class PandaSocialNetworkTest(unittest.TestCase):
                 self.network.add_panda(self.panda)
 
     def test_has_panda(self):
-        new_panda = self._female_panda()
+        panda2 = self._new_panda(1)
         self.network.add_panda(self.panda)
         self.assertTrue(self.network.has_panda(self.panda))
-        self.assertFalse(self.network.has_panda(new_panda))
+        self.assertFalse(self.network.has_panda(panda2))
 
     def test_make_friends(self):
-        new_panda = self._female_panda()
-        self.network.add_friends(self.panda, new_panda)
+        panda2 = self._new_panda(1)
+        self.network.add_friends(self.panda, panda2)
         self.assertEqual(self.network.network,
-                        {self.panda:[new_panda],
-                         new_panda:[self.panda]})
+                        {self.panda:[panda2],
+                         panda2:[self.panda]})
 
     def test_are_friends(self):
-        new_panda = self._female_panda()
-        panda3 = Panda('n', 's@s.s', 'male')
-        self.network.add_friends(self.panda, new_panda)
-        self.assertTrue(self.network.are_friends(self.panda, new_panda))
+        panda2 = self._new_panda(1)
+        panda3 = self._new_panda(2)
+        self.network.add_friends(self.panda, panda2)
+        self.assertTrue(self.network.are_friends(self.panda, panda2))
         self.assertFalse(self.network.are_friends(self.panda, panda3))
 
     def test_friends_of(self):
-        new_panda = self._female_panda()
-        panda3 = Panda('n', 's@s.s', 'male')
-        self.network.add_friends(self.panda, new_panda)
+        panda2 = self._new_panda(1)
+        panda3 = self._new_panda(2)
+        self.network.add_friends(self.panda, panda2)
         self.assertEqual(self.network.friends_of(self.panda),
-                        [new_panda])
+                        [panda2])
         self.assertFalse(self.network.friends_of(panda3))
 
+    def test_connection_level(self):
+        panda2 = self._new_panda(1)
+        panda3 = self._new_panda(2)
+        panda4 = self._new_panda(3)
+        self.network.add_friends(self.panda, panda2)
+        self.network.add_friends(panda2, panda3)
+        self.network.add_friends(panda3, panda4)
+        self.assertEqual(self.network.connection_level(self.panda, panda4), 3)
+        self.network.network = {}
+        self.assertEqual(self.network.connection_level(self.panda, panda2), -1)
+        panda2 = self._new_panda(1)
 
+    def test_are_connected(self):
+        panda2 = self._new_panda(1)
+        panda3 = self._new_panda(2)
+        panda4 = self._new_panda(3)
+        panda5 = self._new_panda(4)
+        self.network.add_friends(self.panda, panda2)
+        self.network.add_friends(panda2, panda3)
+        self.network.add_friends(panda3, panda4)
+        self.assertTrue(self.network.are_connected(self.panda, panda4))
+        self.assertFalse(self.network.are_connected(self.panda, panda5))
 
 
 if __name__ == '__main__':
