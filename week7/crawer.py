b5ddf9d63d2c77081c0adc3729f047858239f450
Nikolai Petkov <binana@abv.bg>
2015-04-29 14:11:31 +0300

    more WIP

--- week7/crawer.py
+++ week7/crawer.py
@@ -1,32 +1,61 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+from histogram import Histogram
+import json
+# import socket
 
 
 class Crawer:
     def __init__(self):
         self.links = []
-        self.browsers = []
+        self.servers = Histogram()
         self.uri = 'http://register.start.bg/'
         self.our_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"}
-        request = requests.get("http://register.start.bg", headers=self.our_headers)
+        request = requests.get("http://register.start.bg",
+                               headers=self.our_headers)
         soup = BeautifulSoup(request.text)
         for link in soup.find_all(href=re.compile('link.php')):
             self.links.append(link.get('href'))
 
     def craw(self):
         for link in self.links:
-            new_url = requests.head(self.uri + link, headers=self.our_headers).headers['Location']
+            new_url = requests.head(self.uri + link,
+                                    headers=self.our_headers).headers['Location']
             try:
-                server = requests.head(new_url, timeout=2).headers['server']
-                self.browsers.append(server)
-                print(server)
-            except requests.exceptions.RequestException:
+                server = requests.head(new_url, timeout=2,
+                                       headers=self.our_headers).headers['server']
+                server = self.clean(server)
+                self.servers.add(server)
+            # except requests.exceptions.RequestException:
+            #     pass
+            # except KeyError:
+            #     pass
+            # except socket.timeout:
+            #     pass
+            except Exception:
                 pass
-            except KeyError:
-                pass
+        self.save()
+
+    def clean(self, server_name):
+        clean_server = server_name
+        if '/' in server_name:
+            clean_server = server_name[0:server_name.index('/')]
+        return clean_server
+
+    def save(self, file_name="crawed_servers.json"):
+        with open(file_name, 'w') as fd:
+            json.dump(self.servers.get_dict(), fd)
+
+    @staticmethod
+    def load(file_name="crawed_servers.json"):
+        with open(file_name, 'r') as fd:
+            hist = Histogram()
+            hist.vault = json.load(fd)
+        return hist
 
 
 if __name__ == '__main__':
     c = Crawer()
     c.craw()
+    c.save()
