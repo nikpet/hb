e9629639ad80e3ee1937ee6093d4570a19f8d655
Nikolai Petkov <binana@abv.bg>
2015-05-12 11:39:13 +0300

    Refactored tests to use strong passwords

--- week9/tests/sql_manager_test.py
+++ week9/tests/sql_manager_test.py
@@ -11,7 +11,7 @@ class SqlManagerTests(unittest.TestCase):
 
     def setUp(self):
         sql_manager.create_clients_table()
-        sql_manager.register('Tester', '123')
+        sql_manager.register('Tester', '!1Aaaaaa')
 
     def tearDown(self):
         sql_manager.cursor.execute('DROP TABLE clients')
@@ -21,15 +21,16 @@ class SqlManagerTests(unittest.TestCase):
         os.remove("bank.db")
 
     def test_register(self):
-        sql_manager.register('Dinko', '123123')
+        self.assertFalse(sql_manager.register('Dinko', '123123'))
+        self.assertTrue(sql_manager.register('Dinko', '!1Aaaaaa'))
 
-        sql_manager.cursor.execute('SELECT Count(*)  FROM clients WHERE username = (?) AND password = (?)', ('Dinko', '123123'))
+        sql_manager.cursor.execute('SELECT Count(*)  FROM clients WHERE username = (?) AND password = (?)', ('Dinko', '!1Aaaaaa'))
         users_count = sql_manager.cursor.fetchone()
 
         self.assertEqual(users_count[0], 1)
 
     def test_login(self):
-        logged_user = sql_manager.login('Tester', '123')
+        logged_user = sql_manager.login('Tester', '!1Aaaaaa')
         self.assertEqual(logged_user.get_username(), 'Tester')
 
     def test_login_injection(self):
@@ -53,13 +54,13 @@ class SqlManagerTests(unittest.TestCase):
         self.assertFalse(logged_user)
 
     def test_change_message(self):
-        logged_user = sql_manager.login('Tester', '123')
+        logged_user = sql_manager.login('Tester', '!1Aaaaaa')
         new_message = "podaivinototam"
         sql_manager.change_message(new_message, logged_user)
         self.assertEqual(logged_user.get_message(), new_message)
 
     def test_change_password(self):
-        logged_user = sql_manager.login('Tester', '123')
+        logged_user = sql_manager.login('Tester', '!1Aaaaaa')
         new_password = "12345"
         self.assertFalse(sql_manager.change_pass(new_password, logged_user))
         strong_pass = "1!Aaaaaa"
