e0de87f97c87370fd255a821f8e6fc6b59e0efe5
Nikolai Petkov <binana@abv.bg>
2015-05-12 11:24:50 +0300

    added is_strong

--- week9/sql_manager.py
+++ week9/sql_manager.py
@@ -1,5 +1,6 @@
 import sqlite3
 from Client import Client
+import re
 
 conn = sqlite3.connect("bank.db")
 cursor = conn.cursor()
@@ -25,6 +26,21 @@ def change_message(new_message, logged_user):
     cursor.execute(update_sql, (new_message, logged_user.get_id()))
     conn.commit()
     logged_user.set_message(new_message)
+
+
+def is_strong(password):
+    if len(password) < 8:
+        return False
+    if not re.search('[a-z]', password):
+        return False
+    if not re.search('[A-Z]', password):
+        return False
+    if not re.search('[0-9]', password):
+        return False
+    if not re.search("""[\~\`\!\@\#\$\%\^\&\*\(\)\_\-\+\=\{
+            \}\[\]\:\;\"\'\\\?\/\>\<\,\.]""", password):
+        return False
+    return True
 
 
 def change_pass(new_pass, logged_user):
@@ -60,3 +76,6 @@ def login(username, password):
         return Client(user[0], user[1], user[2], user[3])
     else:
         return False
+
+if __name__ == '__main__':
+    print(is_strong('adfa*dsf11adfadADSDsf'))
