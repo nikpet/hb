0095209a94dd69cbf856337dbe88c96ae6e4c8ac
Nikolai Petkov <nikipetkov@abv.bg>
2015-06-02 17:57:00 +0300

    added all model to be manageble by admin

--- week10/website/admin.py
+++ week10/website/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Movie, Reservation, Projection
 
 # Register your models here.
+admin.site.register(Movie)
+admin.site.register(Reservation)
+admin.site.register(Projection)
