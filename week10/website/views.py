6429c216bba15a4a84bf962e48ae3a8904a161cc
Nikolai Petkov <nikipetkov@abv.bg>
2015-06-03 19:12:25 +0300

    created index view

--- week10/website/views.py
+++ week10/website/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Projection, Movie
 
 # Create your views here.
+
+
+def index(request):
+    movie_projections = {movie: movie.projection_set.all()
+                    for movie in Movie.objects.all()}.items()
+    return render(request, 'index2.html', locals())
