00a1e7194b84da16d2cffbec6cfe60927f694ed4
Nikolai Petkov <nikipetkov@abv.bg>
2015-06-15 17:25:15 +0300

    added parameter projection_id to make-reservations route

--- week10/website/urls.py
+++ week10/website/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^make-reservation/movie/(?P<movie_id>\d+)/$',
-        views.make_reservation),
+    url(r'^make-reservation/movie/(?P<movie_id>\d+)/projection/(?P<projection_id>\d+)/$',
+        views.make_reservation, name="make_reservation"),
 ]
