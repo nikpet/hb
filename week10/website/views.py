0888734ba2aefc87db9600f5c7bbed3e73eb1bd5
Nikolai Petkov <nikipetkov@abv.bg>
2015-06-15 17:28:55 +0300

    removed import of projection and fixed some identation

--- week10/website/views.py
+++ week10/website/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from .models import Projection, Movie
+from .models import Movie
 
 # Create your views here.
 
 
 def index(request):
     movie_projections = {movie: movie.projection_set.all()
-                    for movie in Movie.objects.all()}.items()
+                         for movie in Movie.objects.all()}.items()
     return render(request, 'index2.html', locals())
