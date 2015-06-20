7da96c76f1d3ef8748c2b49f59456d7f3a4f99d1
Nikolai Petkov <nikipetkov@abv.bg>
2015-06-20 14:10:38 +0300

    added create reservation view

--- week10/website/views.py
+++ week10/website/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Movie
+
+from .models import Movie, Projection
 
 # Create your views here.
 
@@ -8,3 +9,10 @@ def index(request):
     movie_projections = {movie: movie.projection_set.all()
                          for movie in Movie.objects.all()}.items()
     return render(request, 'index2.html', locals())
+
+
+def make_reservation(request, movie_id, projection_id):
+    movie = Movie.objects.get(pk=movie_id)
+    projection = Projection.objects.get(pk=projection_id)
+    form = RoomPlaces(initial={'seats': (1, 2, 3, 4)})
+    return render(request, 'make_reservation.html', locals())
