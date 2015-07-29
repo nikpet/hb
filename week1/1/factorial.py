614cb036edc1eb72e47647a6ba65b39873e6d13a
Nikolai Petkov <binana@abv.bg>
2015-07-29 15:49:57 +0300

    Changed to recursive solutin

--- week1/1/factorial.py
+++ week1/1/factorial.py
@@ -1,8 +1,11 @@
 def factorial(n):
-    result = 1
-    for i in range(1, n + 1):
-        result = result * i
-    return result
+    if n < 1:
+        return 1
+    return n * factorial(n - 1)
+    # result = 1
+    # for i in range(1, n + 1):
+    #     result = result * i
+    # return result
 if __name__ == "__main__":
     print(factorial(0))
     print(factorial(1))
