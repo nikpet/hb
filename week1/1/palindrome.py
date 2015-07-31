48bf05cc9e1bc0a8d3b699fcf4017b20bb669a15
Nikolai Petkov <binana@abv.bg>
2015-07-31 10:47:29 +0300

    Shortened the solution

--- week1/1/palindrome.py
+++ week1/1/palindrome.py
@@ -1,9 +1,8 @@
 def palindrome(obj):
     str_obj = str(obj)
-    first_half = str_obj[:len(str_obj)//2]
-    second_half = str_obj[len(str_obj)//2 + 1:]
-    reverse_second_half = second_half[::-1]
-    return first_half == reverse_second_half
+    return str_obj == str_obj[::-1]
+
+
 if __name__ == "__main__":
     print(palindrome(121))
     print(palindrome("kapak"))
